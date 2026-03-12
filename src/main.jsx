2f150ac3b1b87305df89647e07b415a6f9266fb2
nathanieljulien-a11y <nathanieljulien@gmail.com>
2026-03-12 14:30:05 +0000

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import GardenCalendar from "./GardenCalendar.jsx";
 
+window.__VITE_PROXY_URL__ = import.meta.env.VITE_PROXY_URL || "";
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <GardenCalendar />
